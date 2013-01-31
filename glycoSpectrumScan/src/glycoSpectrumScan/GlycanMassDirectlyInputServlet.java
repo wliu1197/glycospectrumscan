23d689dd760fc1eb1796dd153a2e941598003529
wliu1197 <wliu1197@uni.sydney.edu.au>
2013-01-31 15:57:11 +1100

    with new features

---
+++ glycoSpectrumScan/src/glycoSpectrumScan/GlycanMassDirectlyInputServlet.java
@@ -0,0 +1,48 @@
+package glycoSpectrumScan;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GlycanMassDirectlyInputServlet
+ */
+public class GlycanMassDirectlyInputServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GlycanMassDirectlyInputServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String selected = request.getParameter("Nglycans").toString();
+		
+		request.setAttribute("Number_of_glycans",selected);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("DirectlyInputGlycanMass.jsp");
+		if (dispatcher != null){
+
+			  dispatcher.forward(request, response);
+
+		} 
+	}
+
+}
